4ef4a02a9671b49c4ec25440b021e53e963a737f
Rice <rogergabriel201723@gmail.com>
2026-09-02 20:41:41 -0300

    fix: removing comments

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// Elementos do DOM
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 const navItems = document.querySelectorAll('.nav-links a');
 const header = document.getElementById('main-header');
 
-// Menu Mobile Toggle
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// Fechar menu ao clicar em um link (mobile)
 navItems.forEach(item => {
     item.addEventListener('click', () => {
         navLinks.classList.remove('active');
     });
 });
 
-// Efeito de sombra e fundo no Header ao fazer scroll
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
         header.classList.add('scrolled');
